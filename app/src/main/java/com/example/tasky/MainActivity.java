35bb0dba84ac08ef6a99a361866673770b7292d0
SalvaCM <Salvador.CascadoMa@Elorrieta-Errekamari.com>
2019-11-15 14:16:28 +0100

    sin terminar

--- app/src/main/java/com/example/tasky/MainActivity.java
+++ app/src/main/java/com/example/tasky/MainActivity.java
@@ -2,14 +2,54 @@ package com.example.tasky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText et1,et2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        et1=(EditText)findViewById(R.id.usuario);
+        et2=(EditText)findViewById(R.id.userpass);
+        SharedPreferences preferencias=getSharedPreferences("user",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferencias.edit();
+        editor.putString("Nombre", "Salva");
+        editor.putString("UserPass", "admin");
+        editor.commit();
+    }
+    public void logear(View v) {
+        String nombre=et1.getText().toString();
+        String pass=et2.getText().toString();
+        SharedPreferences prefe=getSharedPreferences("user",
+                Context.MODE_PRIVATE);
+        String nombreenBD=prefe.getString("Nombre","");
+        String passUserBD=prefe.getString("UserPass","");
+        if (nombreenBD.length()==0) {
+            Toast.makeText(this,nombreenBD,
+                    Toast.LENGTH_LONG).show();
+        }
+        else {
+            if(nombre.equals(nombreenBD) && pass.equals(passUserBD))
+            {
+                Toast.makeText(this,"Usuario Correcto",
+                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, TareasActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else
+            {
+                Toast.makeText(this,"Usuario incorrecto",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
