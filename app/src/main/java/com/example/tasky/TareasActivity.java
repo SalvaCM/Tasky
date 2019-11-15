d0162678175c0f76c70d559e3a6203d08d748f5c
SalvaCM <Salvador.CascadoMa@Salvador.CascadoMa@git config --global user.email Salvador.CascadoMa@elorrieta-errekamari.com>
2019-11-15 20:34:12 +0100

    Creado menu

--- app/src/main/java/com/example/tasky/TareasActivity.java
+++ app/src/main/java/com/example/tasky/TareasActivity.java
@@ -3,6 +3,8 @@ package com.example.tasky;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TareasActivity extends AppCompatActivity {
 
@@ -11,4 +13,25 @@ public class TareasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tareas);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.nuevaTarea) {
+           //todo
+        }
+        if (id==R.id.cambiarPass) {
+           //todo
+        }
+        if (id==R.id.acercaDe) {
+          //todo
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
