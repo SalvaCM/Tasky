d8e12cb759d73eb0c303acccd5d3d7e6f3e6d4b2
SalvaCM <Salvador.CascadoMa@Salvador.CascadoMa@git config --global user.email Salvador.CascadoMa@elorrieta-errekamari.com>
2019-11-18 20:01:01 +0100

    subiendo nuevas activitys

---
+++ app/src/main/java/com/example/tasky/ver_tarea.java
@@ -0,0 +1,14 @@
+package com.example.tasky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ver_tarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_tarea);
+    }
+}
